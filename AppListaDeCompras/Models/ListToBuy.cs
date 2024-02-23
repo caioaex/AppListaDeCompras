1e66ac56250c4f497ec0c0bb152bf187b046833d
Caio Augusto Exteckoetter <caioexteckoetter0@gmail.com>
2024-02-23 12:51:05 -0300

    Alterações no projeto

---
+++ AppListaDeCompras/Models/ListToBuy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListaDeCompras.Models
+{
+    class ListToBuy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<User> Users { get; set; } = new List<User>();
+
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
